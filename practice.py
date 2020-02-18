f5b530c477ab88684ab7c24d2c77ad2e8d663193
rshkunov <roman.shkunov@gmail.com>
2020-02-18 18:09:30 +0000

    Added file open to load_data

--- practice.py
+++ practice.py
@@ -3,16 +3,17 @@ import csv
 def hello():
     print("hello")
 
-def load_data(f):
+def load_data(fname):
+    f = open(fname, "r+")
     r = csv.reader(f, delimiter = " ")
     data = []
     for row in r:
         data.append(list(map(int,row)))
+    f.close()
     return (data[0],data[1:][0])
 
 def main(fname):
-    f = open(fname, "r+")
-    params, data = load_data(f)
+    params, data = load_data(fname)
     print(params)
     print(data)
 
