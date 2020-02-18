8e45373806372d02abaed315b752840348eb31c7
rshkunov <roman.shkunov@gmail.com>
2020-02-18 17:54:36 +0000

    Added file read code to practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,19 @@
+import csv
+
 def hello():
     print("hello")
-    
+
+def load_data(f):
+    r = csv.reader(f, delimiter = " ")
+    data = []
+    for row in r:
+        data.append(list(map(int,row)))
+    return (data[0],data[1:][0])
+
+def main(fname):
+    f = open(fname, "r+")
+    params, data = load_data(f)
+    print(params)
+    print(data)
+
+main("a_example.in")
