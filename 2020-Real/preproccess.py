80453a6909ce0bb432affc0d63e4344a3e480208
LucasOJ <lucasodowdjones@gmail.com>
2020-02-20 18:49:38 +0000

    return

--- 2020-Real/preproccess.py
+++ 2020-Real/preproccess.py
@@ -5,6 +5,7 @@ from in_out import *
 def getLibraries(fname):
     (params, bookscores, libs) = load_data(fname)
     timeLim = params[2]
-    libraryList = [library(bookscores,lib[1],,lib[0][1],lib[0][2],i,timeLim)
+    libraryList = [library(bookscores,lib[1],lib[0][1],lib[0][2],i,timeLim)
                     for (i,lib) in enumerate(libs)]
+    return libraryList
 
