030bbfa5b9bf4255a01e1cb357128b21a40c40ef
harry hedges <gk18786@bristol.ac.uk>
2020-02-20 18:23:00 +0000

    new

--- 2020-Real/Library.py
+++ 2020-Real/Library.py
@@ -6,17 +6,19 @@ class book:
 class library:
     bookScores  = []
     bookIndexes = []
+    ID = 0
     numberOfbooks = 0
     signUpTime  = 0
     booksPerDay  = 0
 
 
-    def __init__(self, bookScores , bookIndexes , signUpTime , booksPerDay):
+    def __init__(self, bookScores , bookIndexes , signUpTime , booksPerDay,ID):
         self.bookScores=bookScores
         self.bookIndexes=bookIndexes
         self.signUpTime=signUpTime
         self.booksPerDay=booksPerDay
         self.numberOfbooks = len(bookIndexes)
+        self.ID = ID
     
 
 
