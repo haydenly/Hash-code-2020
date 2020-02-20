617275f4093bc626cd8222198cee3f6d90ab80c0
LucasOJ <lucasodowdjones@gmail.com>
2020-02-20 19:32:34 +0000

    Error correction

--- 2020-Real/Library.py
+++ 2020-Real/Library.py
@@ -24,7 +24,7 @@ class library:
         self.ID = ID
         self.timeLim = timeLim
         self.score = 0
-        self.booksInLib = book(bookScores[index], index) for index in bookIndexes]
+        self.booksInLib = [book(bookScores[index], index) for index in bookIndexes]
     
     def setScore(timeRemaining): 
         daysOfProccessing = timeRemaining - lib.signUpTime
