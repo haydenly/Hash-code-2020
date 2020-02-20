cba0fe379ac764c51a284cf48c2044668b15d7df
LucasOJ <lucasodowdjones@gmail.com>
2020-02-20 19:41:55 +0000

    done

--- 2020-Real/Library.py
+++ 2020-Real/Library.py
@@ -24,8 +24,8 @@ class library:
         self.ID = ID
         self.timeLim = timeLim
         self.score = 0
-        self.booksInLib = sorted([book(bookScores[index], index) for index in bookIndexes], reverse = True, key = lambda x : x.score)
-        self.bookIndexes = list(map(lambda x: x.id, self.booksInLib))
+        booksInLib = sorted([book(bookScores[index], index) for index in bookIndexes], reverse = True, key = lambda x : x.score)
+        self.bookIndexes = list(map(lambda x: x.id, booksInLib))
 
     def setScore(timeRemaining): 
         daysOfProccessing = timeRemaining - lib.signUpTime
