3d679606dc2d400cbbed5abecf0a012c360a8da5
rshkunov <roman.shkunov@gmail.com>
2020-02-20 18:05:19 +0000

    more io, data files added

--- 2020-Real/in_out.py
+++ 2020-Real/in_out.py
@@ -7,9 +7,15 @@ def load_data(fname):
     for row in r:
         data.append(list(map(int,row)))
     f.close()
-    return (data[0],data[1:])
+    params = data[0]
+    bookscores = data[1]
+    libs_raw = data [2:]
+    libs = []
+    for i in range(len(libs_raw)//2):
+        libs.append([libs_raw[i],libs_raw[i+1]]
+    return (params, bookscores, libs)
 
-def out_data(fname, rides):
+def out_data(fname, data):
     o = open(fname, "w")
     w = csv.writer(o, delimiter=" ", lineterminator="\n")
     for ride in rides:
